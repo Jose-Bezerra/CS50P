4d05872f0fbf09611131f5071c7bc2834cb19117
José Bezerra <50915483+Jose-Bezerra@users.noreply.github.com>
2023-10-22 11:10:34 -0300

    Update hello.py
    Refatorando o código

--- hello.py
+++ hello.py
@@ -1,11 +1,5 @@
 # Ask user for their name
-name = input("What's is your name? ")
-
-# Remove whitespace from str
-name = name.strip()
-
-# Capitalize user's name
-name = name.title()
+name = input("What's is your name? ").strip().title()
 
 # Say hello to user
 print(f"hello, {name}")
