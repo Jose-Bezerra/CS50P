aa00cb17521139ccc1269d3cdfa44d8ed0f95232
José Bezerra <50915483+Jose-Bezerra@users.noreply.github.com>
2023-10-22 11:19:24 -0300

    Update hello.py
    Dividindo a string

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 # Ask user for their name
 name = input("What's is your name? ").strip().title()
 
+# Split user's name
+first, last = name.split(" ")
+
 # Say hello to user
-print(f"hello, {name}")
+print(f"hello, {first} {last}")
